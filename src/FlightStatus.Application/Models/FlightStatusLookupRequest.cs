fa651e55f27865252866f204a7b1093130326ddc
heman-kg-68 <hemanthkumargundumilli@gmail.com>
2026-07-20 10:35:19 +0530

    Removed unused files

--- src/FlightStatus.Application/Models/FlightStatusLookupRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace FlightStatus.Application.Models;
-
-public class FlightStatusLookupRequest
-{
-    public string FlightNumber { get; set; } = string.Empty;
-    public string Date { get; set; } = string.Empty;
-}
--- src/FlightStatus.Application/Models/FlightStatusLookupResponse.cs
+++
@@ -1,9 +0,0 @@
-using FlightStatus.Domain.Models;
-
-namespace FlightStatus.Application.Models;
-
-public class FlightStatusLookupResponse
-{
-    public FlightStatusResult? Result { get; set; }
-    public string? Error { get; set; }
-}
